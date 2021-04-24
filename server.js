8fefd8f30c0eb2c091f9ae96f6e7dec435c7ec5d
Ben Hopkins <bjhops17@gmail.com>
2021-04-24 09:25:41 -0700

    please work

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const app = express();
-const cors = require("cors");
-const db = require("./models");
+// const cors = require("cors");
+const placeholder = require("./routes/liked-routes.js")
+const html = require("./routes/html")
+
 // Sets up the Express App
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 
 
 // Sets up the Express app to handle data parsing
@@ -15,11 +17,15 @@ app.use(express.json());
 //   origin: 'https://bjh-hop-estore.herokuapp.com'
 // }
 // corsOptions
-app.use(cors());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Static directory
-app.use(express.static("public"));
+
 /////////////////////////////////
-const placeholder = require("./routes/liked-routes.js")
+
+app.use(placeholder)
+app.use(html)
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/elDoggo",
@@ -32,7 +38,7 @@ mongoose.connect(
 
 // Routes
 // =============================================================
-app.use(placeholder)
+
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 
