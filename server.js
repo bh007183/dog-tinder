4e99e86053fe6b2fb6a5aecb4821c5aed1e7b33a
Ben Hopkins <bjhops17@gmail.com>
2021-04-26 15:15:34 -0700

    last try of the day

--- server.js
+++ server.js
@@ -25,12 +25,14 @@ app.use(placeholder)
 
 
 // ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
+
+  app.use(express.static("client/build"));
+
 
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 
